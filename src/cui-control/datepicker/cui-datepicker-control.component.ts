3b1672f514c9b6e27be8c2834d18edb71447faf8
Arhangelski Nikolay <arhangelskinikolay@gmail.com>
2019-10-15 17:08:08 +0300

    Fixed datepicker

--- src/cui-control/datepicker/cui-datepicker-control.component.ts
+++ src/cui-control/datepicker/cui-datepicker-control.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {CuiControlComponent} from '../cui-control.component';
 import {CuiModelHelper} from "../../services/cui/cui.helper";
 
@@ -6,11 +6,26 @@ import {CuiModelHelper} from "../../services/cui/cui.helper";
     selector: 'cui-datepicker-control',
     templateUrl: './cui-datepicker-control.component.html'
 })
-export class CuiDatepickerControlComponent extends CuiControlComponent {
+export class CuiDatepickerControlComponent extends CuiControlComponent implements OnInit, OnChanges {
   public minDate = '1900-01-01T00:00:00';
   public maxDate = '2100-01-01T00:00:00';
 
   constructor(public cuiModelHelper: CuiModelHelper) {
     super(cuiModelHelper);
+  }
+
+  ngOnInit(): void {
+    this.FormatValue();
+    super.ngOnInit();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.FormatValue()
+  }
+
+  FormatValue() {
+    if (this.value) {
+      this.value = this.value.toString().substr(0, 10);
+    }
   }
 }
