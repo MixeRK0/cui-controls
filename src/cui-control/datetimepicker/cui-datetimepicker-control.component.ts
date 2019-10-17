dab7f8bdfeef3201fdea792879787426659818cf
Arhangelski Nikolay <arhangelskinikolay@gmail.com>
2019-10-17 21:12:53 +0300

    Fixed extends for datetimepicker

--- src/cui-control/datetimepicker/cui-datetimepicker-control.component.ts
+++ src/cui-control/datetimepicker/cui-datetimepicker-control.component.ts
@@ -1,12 +1,15 @@
 import {Component} from '@angular/core';
-import {CuiDatepickerControlComponent} from '../datepicker/cui-datepicker-control.component';
 import {CuiModelHelper} from "../../services/cui/cui.helper";
+import {CuiControlComponent} from '../cui-control.component';
 
 @Component({
   selector: 'cui-datetimepicker-control',
   templateUrl: './cui-datetimepicker-control.component.html'
 })
-export class CuiDatetimepickerControlComponent extends CuiDatepickerControlComponent {
+export class CuiDatetimepickerControlComponent extends CuiControlComponent {
+  public minDate = '1900-01-01T00:00:00';
+  public maxDate = '2100-01-01T00:00:00';
+
   constructor(public cuiModelHelper: CuiModelHelper) {
     super(cuiModelHelper);
   }
